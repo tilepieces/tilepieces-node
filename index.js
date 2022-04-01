991c64a3c500507892102507c41c1efc1c4bf473
tilepieces <90853332+tilepieces@users.noreply.github.com>
2022-04-01 18:59:16 +0200

    version 0.1.5

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-console.log("This is tilepieces version 0.1.3");
+console.log("This is index.js tilepieces version 0.1.4");
 const path = require("path");
 const pathPase = __dirname + path.sep;
 const settingsPath = pathPase + "settings.json";

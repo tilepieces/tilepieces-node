40652820fddb7ba0e166a779edf3ce7892e8bf36
tilepieces <90853332+tilepieces@users.noreply.github.com>
2022-04-05 16:28:25 +0200

    version 0.1.8

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-console.log("This is index.js tilepieces version 0.1.7");
+console.log("This is index.js tilepieces version 0.1.8");
 const path = require("path");
 const pathPase = __dirname + path.sep;
 const settingsPath = pathPase + "settings.json";
--- startTilepiecesServer.js
+++ startTilepiecesServer.js
@@ -1,4 +1,4 @@
-console.log("This is startTilepiecesServer.js tilepieces version 0.1.7");
+console.log("This is startTilepiecesServer.js tilepieces version 0.1.8");
 const server = require("@tilepieces-official/node-server");
 const open = require('open');
 function startServer(settings,basePath) {
